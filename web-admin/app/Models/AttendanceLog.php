f1c8d825e2fe30912bba59bf29963798b111de8a
Titan Anggaraksa <detitanbwi@gmail.com>
2026-03-04 12:27:54 +0700

    Update AttendanceLog model

---
+++ web-admin/app/Models/AttendanceLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceLog extends Model
+{
+    protected $fillable = [
+        'face_registration_id',
+        'check_in_time',
+    ];
+
+    protected $casts = [
+        'check_in_time' => 'datetime',
+    ];
+
+    public function faceRegistration()
+    {
+        return $this->belongsTo(FaceRegistration::class);
+    }
+}
